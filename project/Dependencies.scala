ddb03edaf7b509d42093a7fd14f9651598ba8220
Borislav Borisov <chmodas@astutebits.com>
2020-05-07 20:43:55 +0100

    Project scaffolding
    * The main dependencies
    * Journal schema for Postgres
    * Docker Compose configuration for testing
    * Basic logback configuration

---
+++ project/Dependencies.scala
@@ -0,0 +1,26 @@
+import sbt._
+
+object Versions {
+  lazy val akka = "2.6.4"
+  lazy val r2dbc = "0.8.2.RELEASE"
+  lazy val scalaTest = "3.1.1"
+}
+
+object Dependencies {
+
+  val Libraries = Seq(
+    "com.typesafe.akka" %% "akka-actor-typed" % Versions.akka,
+    "com.typesafe.akka" %% "akka-stream-typed" % Versions.akka,
+    "com.typesafe.akka" %% "akka-persistence-typed" % Versions.akka,
+    "com.typesafe.akka" %% "akka-persistence-query" % Versions.akka,
+
+    "io.r2dbc" % "r2dbc-postgresql" % Versions.r2dbc,
+
+    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
+    "org.scalatest" %% "scalatest" % Versions.scalaTest % Test,
+    "com.typesafe.akka" %% "akka-testkit" % Versions.akka % Test,
+    "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka % Test,
+    "com.typesafe.akka" %% "akka-persistence-tck" % Versions.akka % Test
+  )
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+// Docker Compose
+addSbtPlugin("com.tapad" % "sbt-docker-compose" % "1.0.34")
