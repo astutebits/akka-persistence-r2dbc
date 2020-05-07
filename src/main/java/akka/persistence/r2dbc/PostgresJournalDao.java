2503439c25d7e89ad926afc7ee25cbbc7ca4cba7
Borislav Borisov <chmodas@astutebits.com>
2020-05-07 21:31:10 +0100

    Implement the Journal API with r2dbc-postgresql

---
+++ src/main/java/akka/persistence/r2dbc/PostgresJournalDao.java
@@ -0,0 +1,139 @@
+package akka.persistence.r2dbc;
+
+import static akka.persistence.r2dbc.JournalStatements.deleteEventsQuery;
+import static akka.persistence.r2dbc.JournalStatements.findEventsQuery;
+import static akka.persistence.r2dbc.JournalStatements.highestMarkedSeqNrQuery;
+import static akka.persistence.r2dbc.JournalStatements.highestSeqNrQuery;
+import static akka.persistence.r2dbc.JournalStatements.insertEventsQuery;
+import static akka.persistence.r2dbc.JournalStatements.insertTagsQuery;
+import static akka.persistence.r2dbc.JournalStatements.markEventsAsDeleted;
+
+import akka.Done;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.postgresql.api.PostgresqlResult;
+import java.util.List;
+import java.util.stream.Collectors;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import scala.concurrent.Promise;
+import scala.jdk.javaapi.CollectionConverters;
+
+public final class PostgresJournalDao {
+
+  private final PostgresqlConnectionFactory factory;
+
+  public PostgresJournalDao(PostgresqlConnectionFactory factory) {
+    this.factory = factory;
+  }
+
+  /**
+   * Persists the journal events and any associated tags, and then uses the given promise to signal
+   * back success of failure
+   *
+   * @param events the journal events
+   * @param promise signal back promise
+   */
+  public Flux<Integer> writeEvents(List<JournalEntry> events, Promise<Done> promise) {
+    return factory.create()
+        .flatMapMany(connection -> connection.beginTransaction()
+            .thenMany(
+                connection.createStatement(insertEventsQuery(events))
+                    .execute()
+                    .flatMap(result -> result.map((row, metadata) ->
+                        row.get("ordering", Long.class))
+                    )
+                    .zipWithIterable(events.stream()
+                        .map(event -> CollectionConverters.asJava(event.tags()))
+                        .collect(Collectors.toList())
+                    )
+                    .collectList()
+                    .filter(x -> x.stream().map(Tuple2::getT2).anyMatch(z -> !z.isEmpty()))
+                    .flatMapMany(eventTags -> connection.createStatement(insertTagsQuery(eventTags))
+                        .execute()
+                        .flatMap(PostgresqlResult::getRowsUpdated)
+                    )
+            )
+            .concatWith(Flux.from(connection.commitTransaction())
+                .doOnTerminate(() -> promise.success(Done.done()))
+                .thenMany(Flux.from(connection.close()))
+                .then(Mono.empty()))
+            .onErrorResume(throwable -> Flux.from(connection.rollbackTransaction())
+                .doOnTerminate(() -> promise.failure(throwable))
+                .thenMany(Flux.from(connection.close()))
+                .then(Mono.error(throwable))
+            )
+        );
+  }
+
+  public Flux<JournalEntry> fetchEvents(String persistenceId, Long fromSeqNr, Long toSeqNr) {
+    return factory.create().flatMapMany(connection ->
+        connection.createStatement(findEventsQuery(persistenceId, fromSeqNr, toSeqNr)).execute()
+            .flatMap(result -> result.map((row, metadata) -> JournalEntry.of(
+                row.get("ordering", Long.class),
+                row.get("persistence_id", String.class),
+                row.get("sequence_number", Long.class),
+                row.get("message", byte[].class)
+            )))
+            .concatWith(Flux.from(connection.close()).then(Mono.empty()))
+            .onErrorResume(throwable -> Flux.from(connection.close()).then(Mono.error(throwable)))
+    );
+  }
+
+  /**
+   * Deletes the journal events for the given persistent ID up to the provided sequence number
+   * (inclusive).
+   *
+   * As required by the API, the message deletion doesn't affect the highest sequence number of
+   * messages, and we maintain the highest sequence number by "marking" the journal event entry as
+   * deleted. Instead of deleting it permanently.
+   *
+   * @param persistenceId the persistence id
+   * @param toSequenceNr sequence number (inclusive)
+   */
+  public Flux<Integer> deleteEvents(String persistenceId, Long toSequenceNr) {
+    return factory.create().flatMapMany(connection -> connection.beginTransaction()
+        .thenMany(
+            connection.createStatement(markEventsAsDeleted(persistenceId, toSequenceNr)).execute()
+                .flatMap(__ ->
+                    connection.createStatement(highestMarkedSeqNrQuery(persistenceId))
+                        .execute()
+                        .flatMap(result -> result.map((row, metadata) ->
+                            row.get("sequence_number", Long.class))
+                        )
+                )
+                .defaultIfEmpty(0L)
+                .flatMap(seqNr ->
+                    connection.createStatement(deleteEventsQuery(persistenceId, seqNr - 1))
+                        .execute()
+                )
+                .flatMap(PostgresqlResult::getRowsUpdated)
+        )
+        .concatWith(Flux.from(connection.commitTransaction())
+            .thenMany(Flux.from(connection.close()))
+            .then(Mono.empty()))
+        .onErrorResume(throwable -> Flux.from(connection.rollbackTransaction())
+            .thenMany(Flux.from(connection.close()))
+            .then(Mono.error(throwable)))
+    );
+  }
+
+  /**
+   * Selects the event with the highest {@code sequence_number} whose {@code deleted} set to true.
+   *
+   * @param persistenceId the persistence ID
+   * @param fromSequenceNr sequence number (inclusive)
+   */
+  public Flux<Long> readHighestSequenceNr(String persistenceId, Long fromSequenceNr) {
+    return factory.create().flatMapMany(connection ->
+        connection.createStatement(highestSeqNrQuery(persistenceId, fromSequenceNr)).execute()
+            .flatMap(result -> result.map(((row, metadata) ->
+                row.get("sequence_number", Long.class)))
+            )
+            .defaultIfEmpty(0L)
+            .concatWith(Flux.from(connection.close()).then(Mono.empty()))
+            .onErrorResume(throwable -> Flux.from(connection.close()).then(Mono.error(throwable)))
+    );
+  }
+
+}
---
+++ src/main/scala/akka/persistence/r2dbc/TrySeq.scala
@@ -0,0 +1,46 @@
+package akka.persistence.r2dbc
+
+import scala.collection.immutable.Nil
+import scala.util.{Failure, Success, Try}
+
+private[akka] object TrySeq {
+
+  /**
+   * Flatten a collection of try's to give either a success of the try values, or just the failure.
+   *
+   * @param seq the collection
+   * @tparam A value's type
+   * @return success of the try values or failure
+   */
+  def flatten[A](seq: Seq[Try[A]]): Try[Seq[A]] = {
+    @scala.annotation.tailrec
+    def go(remaining: Seq[Try[A]], processed: Seq[A]): Try[Seq[A]] = remaining match {
+      case Seq() =>
+        Success(processed)
+      case Success(head) +: tail =>
+        go(remaining = tail, processed :+ head)
+      case Failure(t) +: _ =>
+        Failure(t)
+    }
+
+    go(seq, Vector.empty)
+  }
+
+  /**
+   * Creates the signal for a successful write as per the Journal API specification:
+   *
+   * The journal can also signal that it rejects individual messages (`AtomicWrite`) by the
+   * returned `immutable.Seq[Try[Unit]]`. It is possible but not mandatory to reduce number of
+   * allocations by returning `Nil` for the happy path, i.e. when no messages are rejected.
+   * Otherwise the returned `Seq` must have as many elements as the input `messages` `Seq`.
+   * Each `Try` element signals if the corresponding `AtomicWrite` is rejected or not,
+   * with an exception describing the problem.
+   *
+   * @param xs the serialized writes
+   * @tparam A the internal AtomicWrite serialized type
+   * @return Nil if all writes serialization was successful, otherwise a `Seq` with `Try`s
+   */
+  def writeCompleteSignal[A](xs: Seq[Try[A]]): Seq[Try[Unit]] =
+    if (xs.forall(_.isSuccess)) Nil else xs.map(_.map(_ => ()))
+
+}
