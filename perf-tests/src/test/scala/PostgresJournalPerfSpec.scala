cd3202f6207791714028fb3441097d70cf3af7ee
Borislav Borisov <chmodas@astutebits.com>
2020-05-10 12:04:14 +0100

    Add performance tests
    Intended for local execution only.

---
+++ perf-tests/src/test/scala/PostgresJournalPerfSpec.scala
@@ -0,0 +1,47 @@
+import akka.persistence.CapabilityFlag
+import akka.persistence.journal.JournalPerfSpec
+import com.typesafe.config.ConfigFactory
+import io.r2dbc.postgresql.{PostgresqlConnectionConfiguration, PostgresqlConnectionFactory}
+import org.scalatest.concurrent.Eventually
+import reactor.core.publisher.{Flux, Mono}
+import scala.concurrent.duration._
+
+class PostgresJournalPerfSpec
+    extends JournalPerfSpec(config = PostgresJournalPerfSpec.PluginConfig)
+        with Eventually {
+
+  override def beforeAll(): Unit = {
+    eventually(timeout(60.seconds), interval(1.second)) {
+      val cf = new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder()
+          .host("localhost")
+          .username("postgres")
+          .password("s3cr3t")
+          .database("db")
+          .build())
+      cf.create.flatMapMany(connection => {
+        connection.createBatch()
+            .add("DELETE FROM journal_event")
+            .add("DELETE FROM tag")
+            .execute().flatMap(_.getRowsUpdated())
+            .concatWith(Flux.from(connection.close).`then`(Mono.empty()))
+            .onErrorResume(ex => Flux.from(connection.close).`then`(Mono.error(ex)))
+      })
+          .blockLast()
+    }
+
+    super.beforeAll()
+  }
+
+  override def eventsCount: Int = 1000
+
+  override def awaitDurationMillis: Long = 1.minutes.toMillis
+
+  override protected def supportsRejectingNonSerializableObjects: CapabilityFlag = true
+
+}
+object PostgresJournalPerfSpec {
+  val PluginConfig = ConfigFactory.parseString(
+    """
+      |akka.persistence.journal.plugin = "postgres-journal"
+      |""".stripMargin)
+}
