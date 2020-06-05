00c821e81db225195ff6d5e1fefc3dde1ac40e0a
Borislav Borisov <chmodas@astutebits.com>
2020-06-05 23:37:18 +0100

    EventsByPersistenceIdStage for 'eventsByPersistenceId' queries

--- core/src/main/scala/akka/persistence/r2dbc/query/QueryDao.scala
+++ core/src/main/scala/akka/persistence/r2dbc/query/QueryDao.scala
@@ -7,6 +7,21 @@ import akka.stream.scaladsl.Source
 trait QueryDao {
 
   /**
+   * Returns a [[Source]] with a subset of [[JournalEntry]] in the selected `sequenceNr` range for
+   * the given `persistenceId`.
+   *
+   * @param persistenceId the persistence id.
+   * @param fromSeqNr from sequence number (inclusive).
+   * @param toSeqNr to sequence number (inclusive).
+   * @return
+   */
+  def fetchByPersistenceId(
+      persistenceId: String,
+      fromSeqNr: Long,
+      toSeqNr: Long
+  ): Source[JournalEntry, NotUsed]
+
+  /**
    * Returns a [[Source]] with [[JournalEntry]] with sequence numbers in the selected range for the
    * given `tag`.
    *
@@ -24,5 +39,13 @@ trait QueryDao {
    * @return a single element
    */
   def findHighestIndex(tag: String): Source[Long, NotUsed]
+
+  /**
+   * Returns a single element [[Source]] with the highest `sequenceNr` for the given `persistenceId`.
+   *
+   * @param persistenceId the persistence id.
+   * @return a single element
+   */
+  def findHighestSeq(persistenceId: String): Source[Long, NotUsed]
 
 }
