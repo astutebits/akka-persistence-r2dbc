3b331dc03dbd27ab7b1b9ea3e160bc71b00ab9bd
Borislav Borisov <chmodas@astutebits.com>
2020-06-06 22:30:21 +0100

    Abstract EventByStage
    Removes code duplication in EventsByTag and EventsByPersistenceIdStage.

--- core/src/main/scala/akka/persistence/r2dbc/query/QueryDao.scala
+++ core/src/main/scala/akka/persistence/r2dbc/query/QueryDao.scala
@@ -4,7 +4,7 @@ import akka.NotUsed
 import akka.persistence.r2dbc.journal.JournalEntry
 import akka.stream.scaladsl.Source
 
-trait QueryDao {
+private[akka] trait QueryDao {
 
   /**
    * Returns a [[Source]] with a subset of [[JournalEntry]] in the selected `sequenceNr` range for
@@ -13,7 +13,7 @@ trait QueryDao {
    * @param persistenceId the persistence id.
    * @param fromSeqNr from sequence number (inclusive).
    * @param toSeqNr to sequence number (inclusive).
-   * @return
+   * @return a [[Source]] with [[JournalEntry]]s matching the criteria.
    */
   def fetchByPersistenceId(
       persistenceId: String,
@@ -22,29 +22,31 @@ trait QueryDao {
   ): Source[JournalEntry, NotUsed]
 
   /**
-   * Returns a [[Source]] with [[JournalEntry]] with sequence numbers in the selected range for the
-   * given `tag`.
+   * Returns a [[Source]] with a subset of [[JournalEntry]] with record `index`es in the selected
+   * range for the given `tag`.
    *
-   * @param tag the tag
-   * @param fromSeqNr from sequence number (inclusive)
-   * @param toSeqNr to sequence number (inclusive)
-   * @return
+   * @param tag the tag.
+   * @param fromIndex from index (inclusive).
+   * @param toIndex to index (inclusive).
+   * @return a [[Source]] with [[JournalEntry]]s matching the criteria.
    */
-  def fetchByTag(tag: String, fromSeqNr: Long, toSeqNr: Long): Source[JournalEntry, NotUsed]
+  def fetchByTag(tag: String, fromIndex: Long, toIndex: Long): Source[JournalEntry, NotUsed]
 
   /**
-   * Returns a single element [[Source]] with the highest index number for the given tag.
+   * Returns a single element [[Source]] with the highest record index for the given `tag`, or 0 if
+   * a record is not found.
    *
-   * @param tag the tag
-   * @return a single element
+   * @param tag the tag.
+   * @return a [[Source]] with a single [[Long]] element.
    */
   def findHighestIndex(tag: String): Source[Long, NotUsed]
 
   /**
-   * Returns a single element [[Source]] with the highest `sequenceNr` for the given `persistenceId`.
+   * Returns a single element [[Source]] with the highest `sequenceNr` for the given `persistenceId`,
+   * or 0 if a record is not found.
    *
    * @param persistenceId the persistence id.
-   * @return a single element
+   * @return a [[Source]] with a single [[Long]] element.
    */
   def findHighestSeq(persistenceId: String): Source[Long, NotUsed]
 
