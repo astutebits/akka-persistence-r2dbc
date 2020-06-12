947455fc925c552b9e064642486ca7904db9745b
Borislav Borisov <chmodas@astutebits.com>
2020-06-12 15:11:08 +0100

    Fix the EventsByPersistenceId implementation
    The protocol stipulates that you can retrieve a subset of all events by
    specifying `fromSequenceNr` and `toSequenceNr`. However, our
    implementation would keep running, delivering new events as they come
    in, instead of completing the stage successfully.

--- core/src/main/scala/akka/persistence/r2dbc/query/EventsByPersistenceIdStage.scala
+++ core/src/main/scala/akka/persistence/r2dbc/query/EventsByPersistenceIdStage.scala
@@ -3,6 +3,7 @@ package akka.persistence.r2dbc.query
 import akka.NotUsed
 import akka.persistence.r2dbc.journal.JournalEntry
 import akka.stream.scaladsl.Source
+import java.util.concurrent.atomic.AtomicBoolean
 import scala.concurrent.duration.FiniteDuration
 
 private[akka] object EventsByPersistenceIdStage {
@@ -35,20 +36,29 @@ private[akka] final class EventsByPersistenceIdStage private(
   require(toSeqNr >= 0, "the 'toSeqNr' must be >= 0")
   require(fromSeqNr < toSeqNr, "the 'fromSeqNr' must be < the 'toSeqNr'")
 
+  final protected val completeSwitch = new AtomicBoolean(false)
+
+  private var processedEntries: Long = 0
   private var currentSeq: Long = fromSeqNr
   private var targetSeq: Long = 0
 
-  override protected def pushedEntry(entry: JournalEntry): Unit =
+  override protected def pushedEntry(entry: JournalEntry): Unit = {
+    processedEntries += 1
     currentSeq = entry.sequenceNr
+  }
 
   override protected def fetchEvents(): Source[JournalEntry, NotUsed] =
     dao.findHighestSeq(persistenceId)
-        .flatMapConcat(result => {
-          if (targetSeq == result)
+        .flatMapConcat(highestSeq => {
+          if (targetSeq == highestSeq) {
             Source.empty
-          else {
-            targetSeq = if (result > toSeqNr && currentCycle == 0) toSeqNr else result
-            dao.fetchByPersistenceId(persistenceId, if (currentCycle == 0) currentSeq else currentSeq + 1, targetSeq)
+          } else {
+            targetSeq = if (highestSeq >= toSeqNr) {
+              completeSwitch.set(true)
+              toSeqNr
+            } else highestSeq
+
+            dao.fetchByPersistenceId(persistenceId, if (processedEntries == 0) currentSeq else currentSeq + 1, targetSeq)
           }
         })
 
