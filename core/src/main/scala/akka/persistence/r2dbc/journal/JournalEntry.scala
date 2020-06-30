5af9e4328d7f647a23280ed7048effbedff85b9c
Borislav Borisov <chmodas@astutebits.com>
2020-06-30 22:03:46 +0100

    Refactor JournalEntry
    Make `deleted` the last property and set its value to `false` for
    convenience.

--- core/src/main/scala/akka/persistence/r2dbc/journal/JournalEntry.scala
+++ core/src/main/scala/akka/persistence/r2dbc/journal/JournalEntry.scala
@@ -10,18 +10,18 @@ private[akka] object JournalEntry {
       event: Array[Byte],
       tags: Set[String]
   ): JournalEntry =
-    new JournalEntry(Long.MinValue, false, persistenceId, sequenceNr, event, tags)
+    new JournalEntry(Long.MinValue, persistenceId, sequenceNr, event, tags)
 
   def of(index: JLong, persistenceId: String, seqNr: JLong, event: Array[Byte]): JournalEntry =
-    JournalEntry(index.toLong, deleted = false, persistenceId, seqNr.toLong, event)
+    JournalEntry(index.toLong, persistenceId, seqNr.toLong, event)
 
 }
 
 private[akka] final case class JournalEntry(
     index: Long,
-    deleted: Boolean, // XXX: Move the deleted before the tag when adding the serializer ID and stuff
     persistenceId: String,
     sequenceNr: Long,
     event: Array[Byte],
-    tags: Set[String] = Set.empty
+    tags: Set[String] = Set.empty,
+    deleted: Boolean = false
 )
