ccb376cb081ef3cc49baec462269e29cb0e5a7c7
Borislav Borisov <chmodas@astutebits.com>
2020-07-19 13:30:55 +0100

    postgresql: Use the Query TCK

---
+++ postgresql/src/test/scala/akka/persistence/postgresql/query/PostgreSqlReadJournalSpec.scala
@@ -0,0 +1,51 @@
+package akka.persistence.postgresql.query
+
+import akka.persistence.query.{AllPersistenceIdSpec, EventsByPersistenceIdSpec, EventsByTagSpec, ReadJournalSpec}
+import akka.persistence.r2dbc.client.R2dbc
+import com.typesafe.config.ConfigFactory
+import io.r2dbc.postgresql.{PostgresqlConnectionConfiguration, PostgresqlConnectionFactory}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.Eventually
+import scala.concurrent.duration._
+
+object PostgreSqlReadJournalSpec {
+
+  private val Config = ConfigFactory.parseString(
+    """
+      |akka.persistence.journal.plugin = "postgresql-journal"
+      |akka.persistence.publish-plugin-commands = on
+      |akka.actor.allow-java-serialization = on
+      |""".stripMargin)
+
+}
+class PostgreSqlReadJournalSpec
+    extends ReadJournalSpec(PostgreSqlReadJournalSpec.Config)
+        with AllPersistenceIdSpec
+        with EventsByTagSpec
+        with EventsByPersistenceIdSpec
+        with Eventually
+        with BeforeAndAfterEach {
+
+  private val r2dbc = new R2dbc(
+    new PostgresqlConnectionFactory(PostgresqlConnectionConfiguration.builder()
+        .host("localhost")
+        .username("postgres")
+        .password("s3cr3t")
+        .database("db")
+        .build())
+  )
+
+  override def beforeEach(): Unit = {
+    eventually(timeout(60.seconds), interval(1.second)) {
+      r2dbc.withHandle(handle =>
+        handle.executeQuery("DELETE FROM event; DELETE FROM tag;", _.getRowsUpdated)
+      )
+          .blockLast()
+    }
+
+    super.beforeEach()
+  }
+
+  override def pluginId: String = "postgresql-read-journal"
+
+}
