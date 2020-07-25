059fb00ce59e051b17f9eabc51537e59bad2e51a
Borislav Borisov <chmodas@astutebits.com>
2020-07-25 14:43:16 +0100

    Organize the build
    * Remove unused dependencies
    * Ensure proper dependency scope
    * And bump the dependency versions up

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,54 +1,50 @@
 import sbt._
 
 object Versions {
-  lazy val akka = "2.6.4"
-  lazy val r2dbc = "0.8.2.RELEASE"
-  lazy val r2dbcSpi = "0.8.1.RELEASE"
+  lazy val akka = "2.6.8"
+  lazy val reactor = "3.3.6.RELEASE"
+  lazy val r2dbcSpi = "0.8.2.RELEASE"
+  lazy val r2dbcPostgreSql = "0.8.4.RELEASE"
+  lazy val r2dbcMySql = "0.8.2.RELEASE"
   lazy val scalaTest = "3.1.1"
   lazy val mockito = "1.14.2"
 }
 
 object Dependencies {
 
-  val Core = Seq(
-    "io.projectreactor" % "reactor-core" % "3.3.4.RELEASE",
-    "com.typesafe.akka" %% "akka-stream-typed" % Versions.akka,
-    "com.typesafe.akka" %% "akka-persistence-query" % Versions.akka,
-    "io.r2dbc" % "r2dbc-spi" % Versions.r2dbcSpi,
-
-    "org.scalatest" %% "scalatest" % Versions.scalaTest % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % Versions.mockito % Test,
-    "io.r2dbc" % "r2dbc-spi-test" % Versions.r2dbcSpi % Test,
-    "com.typesafe.akka" %% "akka-actor-testkit-typed" % Versions.akka % Test,
-    "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka % Test,
-    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
+  private val Base = Seq(
+    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
+    "org.scalatest" %% "scalatest" % Versions.scalaTest % Test
   )
 
-  private val Libraries = Seq(
-    "com.typesafe.akka" %% "akka-persistence-query" % Versions.akka,
-
-    "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
-    "org.scalatest" %% "scalatest" % Versions.scalaTest % Test,
-    "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka % Test,
+  private val Libraries = Base ++ Seq(
     "com.typesafe.akka" %% "akka-persistence-tck" % Versions.akka % Test
   )
 
+  val Core = Base ++ Seq(
+    "io.projectreactor" % "reactor-core" % Versions.reactor,
+    "com.typesafe.akka" %% "akka-persistence-query" % Versions.akka,
+    "io.r2dbc" % "r2dbc-spi" % Versions.r2dbcSpi,
+
+    "org.mockito" %% "mockito-scala-scalatest" % Versions.mockito % Test,
+    "io.r2dbc" % "r2dbc-spi-test" % Versions.r2dbcSpi % Test,
+    "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka % Test
+  )
+
   val PostgreSQL: Seq[ModuleID] = Libraries ++ Seq(
-    "io.r2dbc" % "r2dbc-postgresql" % Versions.r2dbc
+    "io.r2dbc" % "r2dbc-postgresql" % Versions.r2dbcPostgreSql
   )
 
   val MySQL: Seq[ModuleID] = Libraries ++ Seq(
-    "dev.miku" % "r2dbc-mysql" % "0.8.1.RELEASE"
+    "dev.miku" % "r2dbc-mysql" % Versions.r2dbcMySql
   )
 
   val TCK: Seq[ModuleID] = Seq(
     "com.typesafe.akka" %% "akka-persistence-query" % Versions.akka,
-    "org.scalatest" %% "scalatest" % Versions.scalaTest ,
+    "org.scalatest" %% "scalatest" % Versions.scalaTest,
     "com.typesafe.akka" %% "akka-stream-testkit" % Versions.akka
   )
 
-  val Perf: Seq[ModuleID] = Libraries ++ Seq(
-    "io.r2dbc" % "r2dbc-postgresql" % Versions.r2dbc,
-    "dev.miku" % "r2dbc-mysql" % "0.8.1.RELEASE"
-  )
+  val Perf: Seq[ModuleID] = Libraries
+
 }
