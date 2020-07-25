015cc6cb40a041502d9f9e2fd9edef40be766733
Borislav Borisov <chmodas@astutebits.com>
2020-07-25 14:43:16 +0100

    Organize the build
    * Remove unused dependencies
    * Ensure proper dependency scope
    * And bump the dependency versions up

--- perf-tests/src/test/scala/MySqlJournalPerfSpec.scala
+++ perf-tests/src/test/scala/MySqlJournalPerfSpec.scala
@@ -21,7 +21,7 @@ final class MySqlJournalPerfSpec
             .build())
       )
       r2dbc.withHandle(handle =>
-        handle.executeQuery("DELETE FROM journal_event; DELETE FROM event_tag;", _.getRowsUpdated)
+        handle.executeQuery("DELETE FROM event; DELETE FROM tag;", _.getRowsUpdated)
       )
           .blockLast()
     }
