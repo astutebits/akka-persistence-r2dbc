f0069bc8d93a022e9df8073424c91b8ee48216b0
Borislav Borisov <chmodas@astutebits.com>
2020-08-09 16:08:38 +0100

    Reduce duplication in the plugin configuration

--- core/src/main/scala/akka/persistence/r2dbc/snapshot/SnapshotStorePluginConfig.scala
+++ core/src/main/scala/akka/persistence/r2dbc/snapshot/SnapshotStorePluginConfig.scala
@@ -1,11 +1,11 @@
 package akka.persistence.r2dbc.snapshot
 
-import akka.persistence.r2dbc.journal.JournalPluginConfig
+import akka.persistence.r2dbc.{DatabaseConfig, PluginConfig}
 import com.typesafe.config.Config
 
 private[akka] object SnapshotStorePluginConfig {
   def apply(config: Config): SnapshotStorePluginConfig = new SnapshotStorePluginConfig(config)
 }
-private[akka] final class SnapshotStorePluginConfig(config: Config) extends JournalPluginConfig(config) {
-
+private[akka] final class SnapshotStorePluginConfig(config: Config) extends PluginConfig {
+  override val db: DatabaseConfig = DatabaseConfig(config.getConfig("db"), config.getConfig("db.pool"))
 }
