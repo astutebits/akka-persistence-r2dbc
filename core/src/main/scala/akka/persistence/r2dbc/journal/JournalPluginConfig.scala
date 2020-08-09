054385b3d47fc407c072fc6579eefc8734f89485
Borislav Borisov <chmodas@astutebits.com>
2020-08-09 16:08:38 +0100

    Reduce duplication in the plugin configuration

--- core/src/main/scala/akka/persistence/r2dbc/journal/JournalPluginConfig.scala
+++ core/src/main/scala/akka/persistence/r2dbc/journal/JournalPluginConfig.scala
@@ -1,22 +1,11 @@
 package akka.persistence.r2dbc.journal
 
+import akka.persistence.r2dbc.{DatabaseConfig, PluginConfig}
 import com.typesafe.config.Config
-import java.time.{Duration => JDuration}
 
 private[akka] object JournalPluginConfig {
   def apply(config: Config): JournalPluginConfig = new JournalPluginConfig(config)
 }
-private[akka] class JournalPluginConfig(config: Config) {
-  val hostname: String = config.getString("db.hostname")
-  val port: Int = config.getInt("db.port")
-  val username: String = config.getString("db.username")
-  val password: String = config.getString("db.password")
-  val database: String = config.getString("db.database")
-
-  val poolInitialSize: Int = config.getInt("db.pool.initial-size")
-  val poolMaxSize: Int = config.getInt("db.pool.max-size")
-  val poolMaxLifeTime: JDuration = config.getDuration("db.pool.max-life-time")
-  val poolMaxIdleTime: JDuration = config.getDuration("db.pool.max-idle-time")
-  val poolMaxAcquireTime: JDuration = config.getDuration("db.pool.max-acquire-time")
-  val poolMaxCreateConnectionTime: JDuration = config.getDuration("db.pool.max-create-connection-time")
+private[akka] final class JournalPluginConfig(config: Config) extends PluginConfig {
+  override val db: DatabaseConfig = DatabaseConfig(config.getConfig("db"), config.getConfig("db.pool"))
 }
