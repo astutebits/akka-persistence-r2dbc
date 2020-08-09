92b82300d90282219eb90381b6ba3d9b36fb30bf
Borislav Borisov <chmodas@astutebits.com>
2020-08-09 16:08:38 +0100

    Reduce duplication in the plugin configuration

--- core/src/main/scala/akka/persistence/r2dbc/ConnectionPoolFactory.scala
+++ core/src/main/scala/akka/persistence/r2dbc/ConnectionPoolFactory.scala
@@ -1,39 +1,14 @@
 package akka.persistence.r2dbc
 
-import akka.persistence.r2dbc.journal.JournalPluginConfig
 import io.r2dbc.pool.{ConnectionPool, ConnectionPoolConfiguration}
 import io.r2dbc.spi.{ConnectionFactories, ConnectionFactory, ConnectionFactoryOptions}
 
-trait PluginConfig {
+private[akka] trait PluginConfig {
   val db: DatabaseConfig
 }
 
-private[akka] object ConnectionPoolFactory extends ((String, JournalPluginConfig) => ConnectionFactory) {
+private[akka] object ConnectionPoolFactory extends ((String, PluginConfig) => ConnectionFactory) {
   private val validDrivers = Set("mysql", "postgresql")
-
-  override def apply(driver: String, pluginConfig: JournalPluginConfig): ConnectionFactory = {
-    if (!validDrivers.contains(driver))
-      throw new IllegalArgumentException(s"Driver '$driver' not supported.")
-    val factoryOptions = ConnectionFactoryOptions.builder()
-        .option(ConnectionFactoryOptions.DRIVER, driver)
-        .option(ConnectionFactoryOptions.PROTOCOL, driver)
-        .option(ConnectionFactoryOptions.HOST, pluginConfig.hostname)
-        .option(ConnectionFactoryOptions.PORT, Integer.valueOf(pluginConfig.port))
-        .option(ConnectionFactoryOptions.USER, pluginConfig.username)
-        .option(ConnectionFactoryOptions.PASSWORD, pluginConfig.password)
-        .option(ConnectionFactoryOptions.DATABASE, pluginConfig.database)
-        .build()
-    new ConnectionPool(
-      ConnectionPoolConfiguration.builder(ConnectionFactories.get(factoryOptions))
-          .initialSize(pluginConfig.poolInitialSize)
-          .maxSize(pluginConfig.poolMaxSize)
-          .maxLifeTime(pluginConfig.poolMaxLifeTime)
-          .maxIdleTime(pluginConfig.poolMaxIdleTime)
-          .maxAcquireTime(pluginConfig.poolMaxAcquireTime)
-          .maxCreateConnectionTime(pluginConfig.poolMaxCreateConnectionTime)
-          .build()
-    )
-  }
 
   def apply(driver: String, pluginConfig: PluginConfig): ConnectionFactory = {
     if (!validDrivers.contains(driver))
