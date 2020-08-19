4013908823f5ecc0fadf81024796a5330c12be21
Borislav Borisov <chmodas@astutebits.com>
2020-08-19 22:13:37 +0100

    Apply the Apache 2 license to the code

--- postgresql/src/main/scala/akka/persistence/postgresql/query/PostgreSqlReadJournalQueries.scala
+++ postgresql/src/main/scala/akka/persistence/postgresql/query/PostgreSqlReadJournalQueries.scala
@@ -1,14 +1,24 @@
+/*
+ * Copyright 2020 Borislav Borisov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package akka.persistence.postgresql.query
 
 import java.lang.{Long => JLong}
 
 private[query] object PostgreSqlReadJournalQueries {
-
-  /*
-  def fetchPersistenceIdsQuery(offset: JLong): String =
-    s"SELECT DISTINCT ON (persistence_id) persistence_id, index FROM journal_event " +
-        s" WHERE index >= $offset ORDER BY persistence_id,index DESC"
-   */
 
   def fetchPersistenceIdsQuery(offset: JLong): String =
     s"SELECT persistence_id, max(id) AS id FROM event WHERE id >= $offset" +
