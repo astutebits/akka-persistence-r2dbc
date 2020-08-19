50327e7c993b63fdaee6bb164c2a2013fecff63b
Borislav Borisov <chmodas@astutebits.com>
2020-08-19 22:13:37 +0100

    Apply the Apache 2 license to the code

--- postgresql/src/main/scala/akka/persistence/postgresql/snapshot/PostgreSqlSnapshotStoreQueries.scala
+++ postgresql/src/main/scala/akka/persistence/postgresql/snapshot/PostgreSqlSnapshotStoreQueries.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Borislav Borisov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package akka.persistence.postgresql.snapshot
 
 import akka.persistence.SnapshotSelectionCriteria
@@ -19,13 +35,6 @@ private[akka] object PostgreSqlSnapshotStoreQueries {
   def deleteSnapshotQuery(persistenceId: String, criteria: SnapshotSelectionCriteria): String =
     "DELETE FROM snapshot WHERE persistence_id = '" + persistenceId + "'" + selectionCriteria(criteria)
 
-  def upsertSnapshotQuery(entry: SnapshotEntry): String = {
-    val snapshotHex = "\\x" + ByteBufUtil.hexDump(entry.snapshot)
-    "INSERT INTO snapshot (persistence_id, sequence_nr, instant, snapshot) VALUES " +
-        s"('${entry.persistenceId}', ${entry.sequenceNr}, ${entry.instant}, '$snapshotHex')" +
-        s" ON CONFLICT (persistence_id, sequence_nr) DO UPDATE SET instant = ${entry.instant}, snapshot = '$snapshotHex'"
-  }
-
   private def selectionCriteria(criteria: SnapshotSelectionCriteria) = {
     var query = ""
     if (criteria.maxSequenceNr != Long.MaxValue) query += " AND sequence_nr <= " + criteria.maxSequenceNr
@@ -33,5 +42,12 @@ private[akka] object PostgreSqlSnapshotStoreQueries {
     if (criteria.minSequenceNr != 0L) query += " AND sequence_nr => " + criteria.minSequenceNr
     if (criteria.minTimestamp != 0L) query += " AND instant => " + criteria.minTimestamp
     query
+  }
+
+  def upsertSnapshotQuery(entry: SnapshotEntry): String = {
+    val snapshotHex = "\\x" + ByteBufUtil.hexDump(entry.snapshot)
+    "INSERT INTO snapshot (persistence_id, sequence_nr, instant, snapshot) VALUES " +
+        s"('${entry.persistenceId}', ${entry.sequenceNr}, ${entry.instant}, '$snapshotHex')" +
+        s" ON CONFLICT (persistence_id, sequence_nr) DO UPDATE SET instant = ${entry.instant}, snapshot = '$snapshotHex'"
   }
 }
