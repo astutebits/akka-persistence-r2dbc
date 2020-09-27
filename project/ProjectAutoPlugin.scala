92b3b2f42d381a41964d29dbac2477a8b8b290f3
Borislav Borisov <chmodas@astutebits.com>
2020-09-27 21:09:51 +0100

    Minor changes to the build
    * Run the CI publish task only on merges to master and tag creation
    * Configure the common SBT settings with an auto plugin

---
+++ project/ProjectAutoPlugin.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Borislav Borisov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt.Keys._
+import sbt.plugins.JvmPlugin
+import sbt.{Def, _}
+
+object ProjectAutoPlugin extends AutoPlugin {
+
+  override val requires = JvmPlugin
+
+  override def trigger = allRequirements
+
+  override def globalSettings: Seq[Def.Setting[_]] = Seq(
+    organization := "com.astutebits",
+    organizationName := "AstuteBits",
+    homepage := Some(url("https://github.com/chmodas/akka-persistence-r2dbc")),
+    scmInfo := Some(
+      ScmInfo(
+        url("https://github.com/chmodas/akka-persistence-r2dbc"),
+        "git@github.com:chmodas/akka-persistence-r2dbc"
+      )
+    ),
+    developers += Developer(
+      "contributors",
+      "Contributors",
+      "https://github.com/chmodas/akka-persistence-r2dbc/issues",
+      url("https://github.com/chmodas/akka-persistence-r2dbc/graphs/contributors")
+    ),
+    description := "",
+    startYear := Some(2020)
+  )
+
+  override def projectSettings: Seq[Def.Setting[_]] = Seq(
+    crossVersion := CrossVersion.binary,
+    crossScalaVersions := Versions.supportedScala,
+    scalaVersion := Versions.scala213,
+    scalacOptions ++= Seq(
+      "-encoding", "utf8",
+      "-deprecation",
+      "-unchecked",
+      "-language:implicitConversions",
+      "-language:higherKinds",
+      "-language:existentials",
+      "-language:postfixOps"
+    ),
+    scalacOptions += {
+      if (scalaVersion.value.startsWith("2.13")) "" else "-Ypartial-unification"
+    },
+    javacOptions ++= Seq(
+      "-encoding", "UTF-8",
+      "-deprecation",
+      "-Xlint:unchecked",
+      "-XDignore.symbol.file",
+      "-Xlint:deprecation"
+    ),
+    Test / logBuffered := true,
+    Test / testOptions += Tests.Argument("-oDF")
+
+  )
+}
