f56ddab21df57af8dfdabdc772fd8ce67870516f
Borislav Borisov <chmodas@astutebits.com>
2021-01-03 22:30:16 +0000

    Update the license info

--- core/src/main/scala/akka/persistence/r2dbc/snapshot/ReactiveSnapshotStore.scala
+++ core/src/main/scala/akka/persistence/r2dbc/snapshot/ReactiveSnapshotStore.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Borislav Borisov.
+ * Copyright 2020-2021 Borislav Borisov.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
