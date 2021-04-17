36dacf71edd26d51c969d09c594c1b32c47bbb3b
Scala Steward <me@scala-steward.org>
2021-04-17 02:08:06 +0200

    reactor-core 3.3.16.RELEASE (was 3.3.10.RELEASE)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Versions {
   lazy val supportedScala = Seq(scala213, scala212)
 
   lazy val akka = "2.6.13"
-  lazy val reactor = "3.3.10.RELEASE"
+  lazy val reactor = "3.3.16.RELEASE"
   lazy val r2dbcSpi = "0.8.3.RELEASE"
   lazy val r2dbcPool = "0.8.6.RELEASE"
   lazy val r2dbcPostgreSql = "0.8.7.RELEASE"
