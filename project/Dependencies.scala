89f9f72c7988d2b066ff2dd896464769fa436abe
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-22 11:37:31 +0200

    mockito-scala-scalatest 1.16.46 (was 1.16.39)
    Co-authored-by: Borislav Borisov <chmodas@astutebits.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Versions {
   lazy val r2dbcPostgreSql = "0.8.10.RELEASE"
   lazy val r2dbcMySql = "0.8.2.RELEASE"
   lazy val scalaTest = "3.2.9"
-  lazy val mockito = "1.16.39"
+  lazy val mockito = "1.16.46"
 }
 
 object Dependencies {
