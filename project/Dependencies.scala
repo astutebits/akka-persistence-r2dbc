2a9929e014c5f0ef868fd10e4b6d250e55ab451e
Scala Steward <scala_steward@virtuslab.com>
2022-07-05 19:14:27 +0000

    r2dbc-postgresql 0.8.12.RELEASE (was 0.8.11.RELEASE)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Versions {
   lazy val reactor = "3.4.14"
   lazy val r2dbcSpi = "0.8.6.RELEASE"
   lazy val r2dbcPool = "0.8.8.RELEASE"
-  lazy val r2dbcPostgreSql = "0.8.11.RELEASE"
+  lazy val r2dbcPostgreSql = "0.8.12.RELEASE"
   lazy val r2dbcMySql = "0.8.2.RELEASE"
   lazy val scalaTest = "3.2.11"
   lazy val mockito = "1.16.55"
